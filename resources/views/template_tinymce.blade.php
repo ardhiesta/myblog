3ab5527881425598e7aa3b2de4c75012b2d05db9
ardhiesta <ardhi.mail@gmail.com>
2021-01-17 17:10:05 +0700

    add template with tinymce

---
+++ resources/views/template_tinymce.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tutorial CRUD Laravel 8 untuk Pemula - Ilmucoding.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    
+    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
+	<script>
+		tinymce.init({
+			selector: '#mytextarea'
+		});
+	</script>
+</head>
+<body>
+ 
+<div class="container">
+    @yield('content')
+</div>
+ 
+</body>
+</html>
