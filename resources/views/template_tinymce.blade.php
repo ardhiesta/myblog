e9c190d00795d7e9c34e7844258a11a7ef12dbd0
ardhiesta <ardhi.mail@gmail.com>
2021-01-28 13:55:33 +0700

    change title to My Blog

--- resources/views/template_tinymce.blade.php
+++ resources/views/template_tinymce.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Tutorial CRUD Laravel 8 untuk Pemula - Ilmucoding.com</title>
+    <title>My Blog</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
