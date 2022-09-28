9f583425ec6030a1e812ee4f473fb3bf5d720fec
ardhiesta <ardhi.mail@gmail.com>
2022-09-28 21:28:17 +0700

    migrate to bootstrap 5, edit halaman index posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,50 +1,58 @@
 @extends('template')
  
 @section('content')
-    <div class="row mt-5 mb-5">
-        <div class="col-lg-12 margin-tb">
-            <div class="float-left">
-                <h2>My Blog - Dashboard</h2>
-            </div>
-            <div class="float-right">
-                <a class="btn btn-success" href="{{ route('posts.create') }}"> Create Post</a>
-            </div>
-        </div>
+<div class="navbar">
+    <div class="container">
+        <h2>My Blog - Dashboard</h2>
+
+        <form method="POST" action="{{ route('logout') }}" >
+            @csrf
+            <x-responsive-nav-link :href="route('logout')"
+                onclick="event.preventDefault();
+                this.closest('form').submit();" 
+                class="btn btn-warning">
+                {{ __('Log Out') }}
+            </x-responsive-nav-link>
+        </form>
     </div>
- 
-    @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
+</div>
+
+<div class="row py-lg-2">
+    <div>
+        <a class="btn btn-success" href="{{ route('posts.create') }}"> Create Post</a>
     </div>
-    @endif
+</div>
+     
+@if ($message = Session::get('success'))
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
+@endif
  
-    <table class="table table-bordered">
-        <tr>
-            <th width="20px" class="text-center">No</th>
-            <th>Title</th>
-            <th width="280px"class="text-center">Action</th>
-        </tr>
-        @foreach ($posts as $post)
-        <tr>
-            <td class="text-center">{{ ++$i }}</td>
-            <td>{{ $post->title }}</td>
-            <td class="text-center">
-                <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
+<table class="table table-bordered">
+    <tr>
+        <th width="20px" class="text-center">No</th>
+        <th>Title</th>
+        <th width="280px"class="text-center">Action</th>
+    </tr>
+    @foreach ($posts as $post)
+    <tr>
+        <td class="text-center">{{ ++$i }}</td>
+        <td>{{ $post->title }}</td>
+        <td class="text-center">
+            <form action="{{ route('posts.destroy',$post->id) }}" method="POST">
+                <a class="btn btn-info btn-sm" href="{{ route('posts.show',$post->id) }}">Show</a>
+                <a class="btn btn-primary btn-sm" href="{{ route('posts.edit',$post->id) }}">Edit</a>
+                @csrf
+                @method('DELETE')
+    
+                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
+            </form>
+        </td>
+    </tr>
+    @endforeach
+</table>
  
-                    <a class="btn btn-info btn-sm" href="{{ route('posts.show',$post->id) }}">Show</a>
- 
-                    <a class="btn btn-primary btn-sm" href="{{ route('posts.edit',$post->id) }}">Edit</a>
- 
-                    @csrf
-                    @method('DELETE')
- 
-                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('Apakah Anda yakin ingin menghapus data ini?')">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </table>
- 
-    {!! $posts->links() !!}
+{!! $posts->links() !!}
  
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -2,7 +2,9 @@
 <html>
 <head>
     <title>My Blog</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <!--link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"-->
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
  
